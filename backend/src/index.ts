19dcf900f5c9df6900b12909eefd6ec9417a0316
rawanmohameedd <nrawana12345@gmail.com>
2025-06-07 18:52:49 +0300

    feat: Intialize database connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
+mongoose.connect(process.env.MONGODB_URI!)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(err));
+
 
 app.use(cors());
 app.use(express.json());
